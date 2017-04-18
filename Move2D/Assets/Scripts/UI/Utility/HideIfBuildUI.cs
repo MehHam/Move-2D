003481686c252894b741ef9de24fff415b9cc9f7
Aymeric Faucher <aymeric.faucher@gmail.com>
2017-04-18 18:18:57 +0200

    Webgl UI hiding

--- Move2D/Assets/Scripts/UI/Utility/HideIfBuildUI.cs
+++ Move2D/Assets/Scripts/UI/Utility/HideIfBuildUI.cs
@@ -6,55 +6,56 @@ namespace Move2D
 {
 	public enum BuildType
 	{
-		None = 0,
-		Any = 0xFFFF,
-		WebGL = 1,
-		Mac = 2,
-		Editor = 4,
-		Windows = 8,
-		Linux = 16,
-		IOS = 32,
-		Android = 64,
+		None = 0x00,
+		Any = 0xFF,
+		WebGL = 0x01,
+		Mac = 0x02,
+		Editor = 0x04,
+		Windows = 0x08,
+		Linux = 0x10,
+		IOS = 0x20,
+		Android = 0xFF,
 	}
 
 	public class HideIfBuildUI : MonoBehaviour
 	{
 
 		[BitMask (typeof(BuildType))] public BuildType someMask;
+#if UNITY_EDITOR
+		const int build = 0x04;
 
-		#if UNITY_EDITOR
-		const int build = 4;
-		#elif UNITY_WEBGL
-		const int build = 1;
+#elif UNITY_WEBGL
+		const int build = 0x01;
 		
 
 #elif UNITY_STANDALONE_OSX
-		const int build = 2;
+		const int build = 0x02;
 		
 
 #elif UNITY_STANDALONE_WIN
-		const int build = 8;
+		const int build = 0x04;
 		
 
 #elif UNITY_STANDALONE_LINUX
-		const int build = 16
+		const int build = 0x08;
 		
 
 #elif UNITY_IOS
-		const int build = 32;
+		const int build = 0x10;
 		
 
 #elif UNITY_ANDROID
-		const int build = 64;
+		const int build = 0x20;
 		
 
 #else
-		const int build = 0;
+		const int build = 0x00;
 #endif
 
 		void Start ()
 		{
-		if ((int)someMask == ((int)someMask | (build << (int)someMask))) {
+			Debug.Log (build);
+		if ((int)someMask == ((int)someMask | build)) {
 				var canvas = GetComponent<CanvasGroup> () == null ? this.gameObject.AddComponent<CanvasGroup> () : GetComponent<CanvasGroup> ();
 				canvas.alpha = 0;
 				canvas.interactable = false;
